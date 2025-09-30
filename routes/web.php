82d52774386c73615a53dcac470ee7e3c03d603f
kerin <kerinanggraeni5@gmail.com>
2025-09-30 13:06:14 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
-//use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-    //return ('selamat datang di halaman utama!');
-//});
+Route::get('/', function () {
+    return ('selamat datang di halaman utama!');
+});
+
+Route::get('/hello', function () {
+    return 'Hello, Kerin Anggraeni';
+});
+
+Route::get('user/{name}', function ($name) {
+    return "Nama Saya $name";
+}
+);
+
+Route::get('/greet/{name?}', function ($name = 'Guest') {
+    return "Halo, $name";
+});
+
+Route::get('/profile', function() {
+    return view('profile');
+});
+
+Route::get('about', function() {
+    return view('about', ['name' => 'Kerin Anggraeni']);
+});
+
+Route::get('/home', function () {
+    return 'Halo, Ini adalah halaman Home';
+})->name('home.page');
